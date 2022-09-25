704035b14a270ffea247ba99288b5575210c1e04
Tuan2210 <quangtuan496>
2022-09-25 19:37:34 +0700

    configure mysql db

---
+++ tuan4_Spring_Data_JPA/src/main/java/ius/se/tuan04_Spring_Data_JPA/Tuan04SpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package ius.se.tuan04_Spring_Data_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tuan04SpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tuan04SpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ tuan4_Spring_Data_JPA/src/test/java/ius/se/tuan04_Spring_Data_JPA/Tuan04SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package ius.se.tuan04_Spring_Data_JPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuan04SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
