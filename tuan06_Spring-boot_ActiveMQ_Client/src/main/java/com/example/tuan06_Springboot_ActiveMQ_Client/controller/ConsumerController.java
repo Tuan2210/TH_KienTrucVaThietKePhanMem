1d4de6e40143a5e7e632b0fec467a500b80d51de
Tuan2210 <quangtuan496>
2022-10-07 23:45:00 +0700

    fix

--- tuan06_Spring-boot_ActiveMQ_Client/src/main/java/com/example/tuan06_Springboot_ActiveMQ_Client/controller/ConsumerController.java
+++ tuan06_Spring-boot_ActiveMQ_Client/src/main/java/com/example/tuan06_Springboot_ActiveMQ_Client/controller/ConsumerController.java
@@ -18,32 +18,8 @@ import com.google.gson.GsonBuilder;
 @ComponentScan({"com.example.tuan06_Springboot_JPA_API_Docker_Server.controller"})
 public class ConsumerController {
 	
-//	@Autowired
-//	private ChuyenBayController chuyenBayController;
-//	
-//	@Autowired
-//	private MayBayController mayBayController;
-//	
-//	@Autowired
-//	private NhanVienController nhanVienController;
-	
 	@JmsListener(destination = "${myqueue}")
     public void handleMessage(TextMessage textMessage) throws JMSException {
-		
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		
-//		switch (textMessage.getText()) {
-//		case "http://localhost:8081/api/chuyenbay":
-//			System.out.println("Danh sách chuyến bay: \n" +gson.toJson(textMessage.getText()));
-//			break;
-//		case "http://localhost:8081/api/chuyenbay/gaden:DAD":
-//			System.out.println("Danh sách chuyến bay đến Đà Lạt: \n" +gson.toJson(textMessage.getText()));
-//			break;
-//		default:
-//			System.out.println("Mess received: " +textMessage.getText());
-//			break;
-//		}
-		
 		System.out.println("Mess received => " +textMessage.getText());
     }
 }
