ae5fdf22cc56f25ae5edb2b721409dfa6afc04d5
Tuan2210 <quangtuan496>
2022-10-22 19:17:21 +0700

    create user_post with rest-template

--- tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/NguoiDungController.java
+++ tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/NguoiDungController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +20,19 @@ public class NguoiDungController {
 	@Autowired
 	private NguoiDungRepo nguoiDungRepo;
 	
+	@GetMapping("/nguoidung")
+	public List<NguoiDung_User> listNguoiDung_Users() {
+		return nguoiDungRepo.findAll();
+	}
+	
 	@GetMapping("/nguoidung/{id}")
 	public List<NguoiDung_User> findChuyenBayByGaDen(@PathVariable("id") String mand) {
 		return nguoiDungRepo.findNguoiDungByMaND(mand);
-		
+	}
+	
+	@PostMapping("/nguoidung/add")
+	public NguoiDung_User createUser(@RequestBody NguoiDung_User user) {
+		return nguoiDungRepo.save(user);
+		//thêm trong psotman dùng post, body-raw và chuyển text -> json
 	}
 }
--- tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/RestTemplateController.java
+++ tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/RestTemplateController.java
@@ -6,9 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.example.tuan07_Springboot_RestTemplate_API_Docker.entities.NguoiDung_User;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 
 @RestController
@@ -18,11 +24,26 @@ public class RestTemplateController {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private NguoiDungController nguoiDungController;
+	
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
+	//list chuyenbay
 	@GetMapping("/chuyenbay")
 	public List<Object> getChuyenBays() {
 		String url="http://localhost:8081/api/chuyenbay";
 		
 		Object[] objects = restTemplate.getForObject(url, Object[].class);
+		System.out.println("Danh sách chuyến bay:\n" +gson.toJson(Arrays.asList(objects)));
+		return Arrays.asList(objects);
+	}
+	
+	//list nguoidung-user
+	@GetMapping("/nguoidung")
+	public List<Object> getNguoiDungs() {
+		Object[] objects = restTemplate.getForObject("http://localhost:8081/api/nguoidung", Object[].class);
+		System.out.println("Danh sách người dùng:\n" +gson.toJson(Arrays.asList(objects)));
 		return Arrays.asList(objects);
 	}
 	
@@ -31,6 +52,14 @@ public class RestTemplateController {
 		String url="http://localhost:8081/api/nguoidung/" +mand;
 		
 		Object[] objects = restTemplate.getForObject(url, Object[].class);
+		System.out.println("Người dùng " +mand +":\n" +gson.toJson(Arrays.asList(objects)));
 		return Arrays.asList(objects);
+	}
+	
+	@PostMapping("/nguoidung/add")
+	public Object createUserObj(@RequestBody NguoiDung_User object) {
+		Object userObj = nguoiDungController.createUser(object);
+		System.out.println("Đã thêm người dùng-user:\n" +gson.toJson(userObj));
+		return userObj;
 	}
 }
