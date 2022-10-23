499ca16b17f86881edb409be6ce857b7675a642c
Tuan2210 <quangtuan496>
2022-10-23 11:13:49 +0700

    fix test postman find nguoidung by mand

--- tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/NguoiDungController.java
+++ tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/controllers/NguoiDungController.java
@@ -1,17 +1,22 @@
 package com.example.tuan07_Springboot_RestTemplate_API_Docker.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.tuan07_Springboot_RestTemplate_API_Docker.entities.NguoiDung_User;
 import com.example.tuan07_Springboot_RestTemplate_API_Docker.repositories.NguoiDungRepo;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @RestController
 @RequestMapping("/api")
@@ -20,14 +25,23 @@ public class NguoiDungController {
 	@Autowired
 	private NguoiDungRepo nguoiDungRepo;
 	
+	Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	
 	@GetMapping("/nguoidung")
 	public List<NguoiDung_User> listNguoiDung_Users() {
 		return nguoiDungRepo.findAll();
 	}
 	
+//	@GetMapping("/nguoidung/{id}")
+//	public List<NguoiDung_User> findChuyenBayByGaDen(@PathVariable("id") String mand) {
+//		return nguoiDungRepo.findNguoiDungByMaND(mand);
+//	}
 	@GetMapping("/nguoidung/{id}")
-	public List<NguoiDung_User> findChuyenBayByGaDen(@PathVariable("id") String mand) {
-		return nguoiDungRepo.findNguoiDungByMaND(mand);
+	public ResponseEntity<NguoiDung_User> getNguoiDungByMaND(@PathVariable("id") String mand) throws Exception {
+		NguoiDung_User nguoiDung_User = nguoiDungRepo.findById(mand)
+													 .orElseThrow(() -> new Exception("Ko tìm thấy người dùng có id là " +mand));
+		System.out.println("Người dùng id là " +mand +":\n" +gson.toJson(nguoiDung_User));
+		return ResponseEntity.ok().body(nguoiDung_User);
 	}
 	
 	@PostMapping("/nguoidung/add")
@@ -35,4 +49,10 @@ public class NguoiDungController {
 		return nguoiDungRepo.save(user);
 		//thêm trong psotman dùng post, body-raw và chuyển text -> json
 	}
+	
+//	@PutMapping("/nguoidung/update:{id}")
+//	public NguoiDung_User updateUser(@PathVariable("id") String mand, @RequestBody NguoiDung_User user) {
+//		return nguoiDungRepo.save(nguoiDungRepo.findById(mand));
+//		//sửa trong psotman dùng post, body-raw và chuyển text -> json
+//	}
 }
--- tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/repositories/NguoiDungRepo.java
+++ tuan07_Spring-boot_RestTemplate_API_Docker/src/main/java/com/example/tuan07_Springboot_RestTemplate_API_Docker/repositories/NguoiDungRepo.java
@@ -1,18 +1,17 @@
 package com.example.tuan07_Springboot_RestTemplate_API_Docker.repositories;
 
-import java.util.List;
+//import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.example.tuan07_Springboot_RestTemplate_API_Docker.entities.ChuyenBay;
 import com.example.tuan07_Springboot_RestTemplate_API_Docker.entities.NguoiDung_User;
 
 @Repository
 public interface NguoiDungRepo extends JpaRepository<NguoiDung_User, String>{
 	
-	@Query(value = "SELECT * FROM nguoidung where MaND = ?", nativeQuery = true)
-	public List<NguoiDung_User> findNguoiDungByMaND(String mand);
+//	@Query(value = "SELECT * FROM nguoidung where MaND = ?", nativeQuery = true)
+//	public List<NguoiDung_User> findNguoiDungByMaND(String mand);
 
 }
