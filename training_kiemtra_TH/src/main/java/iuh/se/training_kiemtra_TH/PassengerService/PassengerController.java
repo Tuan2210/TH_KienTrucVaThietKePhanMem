c58197fb355241351a0b59c3dfc25dfeaef6512e
Tuan2210 <quangtuan496>
2022-11-03 18:55:20 +0700

    find passenger by id

--- training_kiemtra_TH/src/main/java/iuh/se/training_kiemtra_TH/PassengerService/PassengerController.java
+++ training_kiemtra_TH/src/main/java/iuh/se/training_kiemtra_TH/PassengerService/PassengerController.java
@@ -1,9 +1,13 @@
 package iuh.se.training_kiemtra_TH.PassengerService;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,10 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class PassengerController {
 	
 	@Autowired
-	private PassengerService passengerService;
+	private PassengerServices passengerServices;
 	
 	@GetMapping("/passengers")
 	public List<Passenger> listPassengers() {
-		return passengerService.listPassengers();
+		return passengerServices.listPassengers();
+	}
+	
+	@GetMapping("/passengers/{id}")
+	public Optional<Passenger> findPassengerById(@PathVariable("id") String passId) {
+		return passengerServices.findPassengerById(passId);
+	}
+	
+	@PostMapping("/passengers/add")
+	public Passenger addPassenger(@RequestBody Passenger passenger) {
+		return passengerServices.addPassenger(passenger);
 	}
 }
--- training_kiemtra_TH/src/main/java/iuh/se/training_kiemtra_TH/PassengerService/PassengerServices.java
+++ training_kiemtra_TH/src/main/java/iuh/se/training_kiemtra_TH/PassengerService/PassengerServices.java
@@ -1,17 +1,26 @@
 package iuh.se.training_kiemtra_TH.PassengerService;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class PassengerService {
+public class PassengerServices {
 	
 	@Autowired
 	private PassengerRepository passengerRepository;
 	
 	public List<Passenger> listPassengers() {
 		return passengerRepository.findAll();
+	}
+	
+	public Optional<Passenger> findPassengerById(String passId) {
+		return passengerRepository.findById(passId);
+	}
+	
+	public Passenger addPassenger(Passenger passenger) {
+		return passengerRepository.save(passenger);
 	}
 }
