2c22340f5daabe5e607110c07ffbb3a165fedc34
Tuan2210 <quangtuan496>
2022-12-07 23:15:28 +0700

    add link yt singleton normal

--- demo_DesignPatterns/src/Singleton_Pattern/Test_Singleton.java
+++ demo_DesignPatterns/src/Singleton_Pattern/Test_Singleton.java
@@ -1,14 +1,21 @@
 package Singleton_Pattern;
 
+//link yt: https://www.youtube.com/watch?v=UxdpuhF56NU&list=PLRoAKls-7kksSBqximU9CjBgCApQVDgmX&index=2
+
 public class Test_Singleton {
-	//link yt: https://www.youtube.com/watch?v=UxdpuhF56NU&list=PLRoAKls-7kksSBqximU9CjBgCApQVDgmX&index=2
 	
 	public static void main(String[] args) {
+		System.out.println("singleton pattern:");
+		
 		Eager_Initialization eager1 = Eager_Initialization.getInstance();
 		eager1.setName("John");
 		System.out.println("Eager1: " +eager1.getName());
 		
 		Eager_Initialization eager2 = Eager_Initialization.getInstance();
 		System.out.println("Eager2: " +eager2.getName());
+		
+		System.out.println("====================");
+		
+		
 	}
 }
